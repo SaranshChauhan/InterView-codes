421b6f59a23b5fe2b1e3bfffd8a5c83cb61921e4
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-04-16 14:16:23 +0530

    Create sscanf.c

---
+++ Ckodes/sscanf.c
@@ -0,0 +1,31 @@
+
+#include<stdio.h>
+int main()
+{
+char arr[20] = "User M 19 1.85";
+
+char str[10];
+char ch;
+int i;
+float f;
+
+/* Calling sscanf() to read multiple values from a char[] array and store each value in corresponding formatted variable */
+
+sscanf(ar, "%s %c %d %f", &str, &ch, &i, &f);
+
+
+printf("The value in string is : %s ", str);
+printf("\n");
+
+printf("The value in char is : %c ", ch);
+printf("\n");
+
+printf("The value in int is : %d ", i);
+printf("\n");
+
+printf("The value in float is : %f ", f);
+
+sscanf(ar, "%s %c %d", &str, &ch, &i);
+
+return 0;
+}
