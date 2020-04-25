9f56a891a966389e5b760b0faa96fc6ae638ebcd
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-04-25 23:40:05 +0530

    Create Anagram.c
    An anagram is a word or phrase that's formed by rearranging the letters of another word or phrase. For example, the letters that make up “A decimal point” can be turned into the anagram “I'm a dot in place.” People mainly make anagrams just for fun, but sometimes they're used as pseudonyms or codes.

---
+++ Ckodes/Anagram.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+void anagram(char[]);
+int main() {
+    char *str1="Satr";
+    char *str2="aStr";
+    int val;
+   // if(strlen(str1)==strlen(str2))
+    anagram(str1);
+    anagram(str2);
+     val=strcmp(str1,str2);
+    if(val==0)
+        printf("Anagram");
+    else
+        printf("Not anagram");
+        
+    return 0;
+}
+
+void anagram(char string[20]){
+    int i;
+    char c;
+    for(i=0;i<strlen(string);i++){
+      if(string[i]<string[i+1]){
+        c=string[i];
+        string[i]=string[i+1];
+        string[i+1]=c;
+      }
+   }
+}
